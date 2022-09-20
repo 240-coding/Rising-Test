1930a75d468c660d704b9e309bfc4838b64176dc
Seoyoung Lee <wow9176@naver.com>
2022-09-20 16:05:45 +0900

    feat: 검색, 등록 메뉴 모달 구현

---
+++ RisingTest/Source/Main/MainTabBarController.swift
@@ -0,0 +1,39 @@
+//
+//  MainTabBarController.swift
+//  RisingTest
+//
+//  Created by 이서영 on 2022/09/20.
+//
+
+import UIKit
+
+class MainTabBarController: UITabBarController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+        delegate = self
+    }
+}
+
+extension MainTabBarController: UITabBarControllerDelegate {
+    func tabBarController(_ tabBarController: UITabBarController, shouldSelect viewController: UIViewController) -> Bool {
+        if viewController == tabBarController.viewControllers?[1] {
+            guard let searchViewController = UIStoryboard(name: "MainStoryboard", bundle: nil).instantiateViewController(withIdentifier: "SearchViewController") as? SearchViewController else {
+                return true
+            }
+            searchViewController.modalPresentationStyle = .fullScreen
+            present(searchViewController, animated: true)
+            return false
+        } else if viewController == tabBarController.viewControllers?[2] {
+            guard let writeViewController = UIStoryboard(name: "MainStoryboard", bundle: nil).instantiateViewController(withIdentifier: "WriteViewController") as? WriteViewController else {
+                return true
+            }
+            writeViewController.modalPresentationStyle = .fullScreen
+            present(writeViewController, animated: true)
+            return false
+        }
+        return true
+    }
+}
