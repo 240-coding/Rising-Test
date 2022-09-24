0d4e807f784c94cd5850d8b8ea46dbeacc3e42af
Seoyoung Lee <wow9176@naver.com>
2022-09-24 18:30:56 +0900

    feat: 홈 화면 추천상품 브랜드 메뉴 tabman 활용

---
+++ RisingTest/Source/Main/Home/TabManViewController.swift
@@ -0,0 +1,80 @@
+//
+//  TabManViewController.swift
+//  RisingTest
+//
+//  Created by 이서영 on 2022/09/24.
+//
+
+import UIKit
+import Tabman
+import Pageboy
+
+class TabManViewController: TabmanViewController {
+    
+    var viewControllers = [UIViewController]()
+    
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        setViewControllers()
+        self.dataSource = self
+        
+        let bar = TMBar.ButtonBar()
+        
+        bar.layout.transitionStyle = .snap
+        bar.layout.alignment = .leading
+        bar.layout.contentMode = .intrinsic
+        bar.layout.interButtonSpacing = 24
+        
+        bar.backgroundView.style = .clear
+        bar.backgroundColor = .white
+        bar.layout.contentInset = UIEdgeInsets(top: 0, left: 16, bottom: 0, right: 10)
+        
+        bar.buttons.customize { button in
+            button.tintColor = UIColor(named: "gray")
+            button.selectedTintColor = .black
+            button.font = .boldSystemFont(ofSize: 17)
+        }
+        bar.indicator.weight = .custom(value: 3)
+        bar.indicator.cornerStyle = .rounded
+        bar.indicator.overscrollBehavior = .bounce
+        bar.indicator.tintColor = .black
+
+        addBar(bar, dataSource: self, at:.top)
+    }
+    
+    func setViewControllers() {
+        let storyboard = UIStoryboard(name: "MainStoryboard", bundle: nil)
+        guard let recommendViewController = storyboard.instantiateViewController(withIdentifier: "RecommendViewController") as? RecommendViewController, let brandViewController = storyboard.instantiateViewController(withIdentifier: "BrandViewController") as? BrandViewController else {
+            return
+        }
+        viewControllers = [recommendViewController, brandViewController]
+    }
+
+}
+
+extension TabManViewController: TMBarDataSource, PageboyViewControllerDataSource {
+    func barItem(for bar: TMBar, at index: Int) -> TMBarItemable {
+        switch index {
+        case 0:
+            return TMBarItem(title: "추천상품")
+        case 1:
+            return TMBarItem(title: "브랜드")
+        default:
+            return TMBarItem(title: "")
+        }
+    }
+    
+    func numberOfViewControllers(in pageboyViewController: PageboyViewController) -> Int {
+        return viewControllers.count
+    }
+    
+    func viewController(for pageboyViewController: PageboyViewController, at index: PageboyViewController.PageIndex) -> UIViewController? {
+        return viewControllers[index]
+    }
+    
+    func defaultPage(for pageboyViewController: PageboyViewController) -> PageboyViewController.Page? {
+        return nil
+    }
+}
